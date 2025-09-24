3301bdfe73dc7d8b3bd26b18af4447846d07b4be
sagarbhat-virtuele <sagar.r@virtuele.us>
2025-09-24 18:04:04 +0530

    Implementing cors

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 from core.database import engine, Base
 from routers import blog, user, profile
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
-
-# Base.metadata.drop_all(bind=engine) 
 Base.metadata.create_all(engine)
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(blog.router)
 app.include_router(user.router)
